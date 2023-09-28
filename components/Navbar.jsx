ad8cfe560555ed01596f5ea58f9864cf98144052
Dan Crha <44410856+danielcrha@users.noreply.github.com>
2023-09-28 19:26:10 -0400

    mobile sidebar work

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,12 +1,13 @@
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
+import { AiOutlineClose, AiOutlineMenu } from "react-icons/ai";
 
 const Navbar = () => {
   return (
     <div className='fixed w-full h-20 shadow-xl z-[100]'>
       <div className='flex items-center justify-between w-full h-full px-2 2xl:px-16'>
-        <Image src='/../assets/Logo.png' alt='/' width={150} height={50} />
+        <Image src='/../assets/Logoo.png' alt='/' width={150} height={50} />
         <div>
           <ul className='hidden md:flex'>
             <Link href='/'>
@@ -29,6 +30,51 @@ const Navbar = () => {
               </li>
             </Link>
           </ul>
+          <div className='md:hidden'>
+            <AiOutlineMenu size={25} />
+          </div>
+        </div>
+      </div>
+
+      <div className='fixed top-0 left-0 w-full h-screen bg-black/70'>
+        <div className='fixed left-0 top-0 w-[75%] sm:w-[60%] md:w-[45%] h-screen bg-[#ecf0f3] p-10 ease-in duration-500'>
+          <div>
+            <div className='flex items-center justify-between w-full'>
+              <Image
+                src='/../assets/Logo.png'
+                alt='/'
+                width={150}
+                height={50}
+              />
+              <div className='rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer'>
+                <AiOutlineClose />
+              </div>
+            </div>
+            <div className='border-b border-gray-300 my-4'>
+              <p className='mb-4 text-center w-[85%] md:w-[90%] py-4'>
+                Building software that demonstrates my passion and expertise.
+              </p>
+            </div>
+          </div>
+          <div className='py-4 flex flex-col'>
+            <ul className='uppercase'>
+              <Link href='/'>
+                <li className='py-4 text-sm'>Home</li>
+              </Link>
+              <Link href='/'>
+                <li className='py-4 text-sm'>About</li>
+              </Link>
+              <Link href='/'>
+                <li className='py-4 text-sm'>Skills</li>
+              </Link>
+              <Link href='/'>
+                <li className='py-4 text-sm'>Projects</li>
+              </Link>
+              <Link href='/'>
+                <li className='py-4 text-sm'>Contact</li>
+              </Link>
+            </ul>
+          </div>
         </div>
       </div>
     </div>
