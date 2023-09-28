334fbaaa484e3d8b775ab743b246dc898075d3a8
Dan Crha <44410856+danielcrha@users.noreply.github.com>
2023-09-28 15:16:18 -0400

    navbar work

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,7 +1,43 @@
+import Image from "next/image";
+import Link from "next/link";
 import React from "react";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <div className='fixed w-full h-20 shadow-xl z-[100]'>
+      <div className='flex items-center justify-between w-full h-full px-2 2xl:px-16'>
+        <Image
+          src='/../public/assets/navLogo.png'
+          alt='/'
+          width={125}
+          height={50}
+        />
+        <div>
+          <ul className='hidden md:flex'>
+            <Link href='/'>
+              <li className='ml-10 text-sm uppercase hover:border-b'>Home</li>
+            </Link>
+            <Link href='/'>
+              <li className='ml-10 text-sm uppercase hover:border-b'>About</li>
+            </Link>
+            <Link href='/'>
+              <li className='ml-10 text-sm uppercase hover:border-b'>Skills</li>
+            </Link>
+            <Link href='/'>
+              <li className='ml-10 text-sm uppercase hover:border-b'>
+                Projects
+              </li>
+            </Link>
+            <Link href='/'>
+              <li className='ml-10 text-sm uppercase hover:border-b'>
+                Contact
+              </li>
+            </Link>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
