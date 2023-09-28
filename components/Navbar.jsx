dfd8932176218c20cc763aec99fd9ad68d02bc8f
Dan Crha <44410856+danielcrha@users.noreply.github.com>
2023-09-28 18:47:42 -0400

    navbar work

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -6,12 +6,7 @@ const Navbar = () => {
   return (
     <div className='fixed w-full h-20 shadow-xl z-[100]'>
       <div className='flex items-center justify-between w-full h-full px-2 2xl:px-16'>
-        <Image
-          src='/../public/assets/navLogo.png'
-          alt='/'
-          width={125}
-          height={50}
-        />
+        <Image src='/../assets/Logo.png' alt='/' width={150} height={50} />
         <div>
           <ul className='hidden md:flex'>
             <Link href='/'>
