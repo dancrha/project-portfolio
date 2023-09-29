9df52f7b1ceb838e9a16f7c9bb16bc908a792452
Dan Crha <44410856+danielcrha@users.noreply.github.com>
2023-09-29 13:18:13 -0400

    skills started

--- components/Skills.jsx
+++ components/Skills.jsx
@@ -1,7 +1,34 @@
+import Image from "next/image";
 import React from "react";
 
 const Skills = () => {
-  return <div>Skills</div>;
+  return (
+    <div className='w-full lg:h-screen p-2'>
+      <div className='max-w-[1240px] mx-auto flex flex-col justify-center h-full'>
+        <p className='text-xl tracking-widest uppercase text-[#176B87]'>
+          Skills
+        </p>
+        <h2 className='py-4'>What I Can Do</h2>
+        <div className='grid md:grid-cols-2 lg:grid-cols-4 gap-8'>
+          <div className='p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300'>
+            <div className='grid grid-cols-2 justify-center items-center'>
+              <div className='m-auto'>
+                <Image
+                  src='/../assets/skills/javascript.png'
+                  alt='/'
+                  width={64}
+                  height={64}
+                />
+              </div>
+              <div className='flex flex-col items-center justify-center'>
+                <h3>Javascript</h3>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Skills;
